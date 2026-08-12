37a719956b605eca8c6f90e1f73e963e5a1de35e
salm <qa300220@gmail.com>
2026-08-12 22:20:45 +0300

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = FirebaseOptions(
+    apiKey: 'AIzaSyBDyYqMaGSdfmrZWjvQnM-svhG7YMAUpag',
+    appId: '1:185968406829:web:673cdceadb7dfe989263a4',
+    messagingSenderId: '185968406829',
+    projectId: 'twetlink-349f4',
+    authDomain: 'twetlink-349f4.firebaseapp.com',
+    storageBucket: 'twetlink-349f4.appspot.com',
+    measurementId: 'G-GTZB8B6H0G',
+  );
+}
